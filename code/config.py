f89a6e10327a0117ae644ef30219491a2b8d1cfa
Fuma Ito <fmjp1012@gmail.com>
2025-12-03 16:19:47 +0900

    Merge stash changes, update config.py

--- code/config.py
+++ code/config.py
@@ -320,7 +320,7 @@ CONFIG_MAIN = SimulationConfig(
     # シナリオ共通パラメータ
     common=CommonParams(
         N=50,
-        T=1000,  # テスト用に短くしています
+        T=1000,  # テスト用に短くしています -> 本番パラメータ
         sparsity=0.7,
         max_weight=0.5,
         std_e=0.05,
